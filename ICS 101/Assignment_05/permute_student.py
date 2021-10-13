4369868a466b8bb9f14ea47e5921f661232678b9
Arghya Sarkar <arghya@nyu.edu>
2021-10-13 13:10:03 +0530

    inital homework

---
+++ ICS 101/Assignment_05/permute_student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 10 15:20:57 2021
+
+@author: bing
+"""
+
+def permute(nums):
+    ## put your code here
+    
+    pass
+
+    
+##tests
+
+if __name__ == "__main__":
+    nums = [1, 2, 3]
+    p1 = permute(nums)
+    print("Permutation:", p1)
+    
+    nums = [4, 1, 2, 3]
+    p2 = permute(nums)
+    print("Permutation:", p2)
+    
+    nums = [4, 1, 1, 4]
+    p3 = permute(nums)
+    print("Permutation:", p3)
+    
+
