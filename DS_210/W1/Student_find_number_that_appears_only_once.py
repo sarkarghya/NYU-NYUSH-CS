488a6d35753f3db54f237a9611ff78c7145142b6
sarkarghya <arghya@nyu.edu>
2022-02-07 00:02:11 +0530

    week 1

---
+++ DS_210/W1/Student_find_number_that_appears_only_once.py
@@ -0,0 +1,12 @@
+def find(lissy):
+    # TODo
+    return -1
+
+def main():
+    l1 = [7,1,5,3,6,4,7,1,5,6,4]
+    l2 = [7,6,4,3,2,1,1,2,3,4,5,6,7]
+    print(find(l1)) # expect: 3
+    print(find(l2)) # expect: 5
+
+if __name__ == '__main__':
+    main()
