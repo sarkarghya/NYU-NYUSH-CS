1458bcae9146dc55951ffe13380a2a3c0930043f
sarkarghya <sarkarghya@gmail.com>
2023-09-18 18:37:06 +0800

    HW2 Start

---
+++ OS_215/WS02.start/src/decoder.c
@@ -0,0 +1,60 @@
+/**** decoder.c ****/
+
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <decoder.h>
+
+
+#define _XOPEN_SOURCE 700
+
+
+int magicsq[3][3] = {{4, -1, 8}, {-1, -1, -1}, {2, -1, 6}};
+
+
+int check() {
+	int i, j, sums[8];
+	for (i = 0; i < 8; i++) 
+		sums[i] = 0;
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++) {
+			sums[i] += magicsq[i][j];
+			sums[i+3] += magicsq[j][i];
+			if (i == j) sums[6] += magicsq[i][j];
+			if ((i+j) == 2) sums[7] += magicsq[i][j];
+		}
+	}
+	int result = 1;
+	i = 1;
+	while ((i < 8) && (result == 1)) {
+		if (sums[0] != sums[i])
+			result = 0;
+		i++;
+	}
+	return result;
+}
+
+
+void display() {
+	int i, j;
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 3; j++)
+			printf("  %d", magicsq[i][j]);
+		printf("\n");
+	}
+	printf("\n");
+}
+
+
+int solve(int x) {
+	/* CODE THAT EVERY CHILD PROCESS EXECUTES */
+}
+
+int main(int argc, char **argv)
+{	
+	/* PARENT PROCESS */
+}
